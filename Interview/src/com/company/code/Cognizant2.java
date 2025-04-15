20e3591661358c2f29c22a7670a1e1fec6e266d2
jayaprakashbsjpps <jayaprakashbsjpps@gmail.com>
2025-04-15 14:54:35 +0000

    Cognizant2 problems

---
+++ Interview/src/com/company/code/Cognizant2.java
@@ -0,0 +1,35 @@
+public class Cognizant2 {
+
+    public static void main(String[] args) {
+
+        //Write a java program to find the index positions of a string
+       // For example, Given String "aaabcddccdcbabcddadbaabcda" and input String "bcd" and output is 3,13,22
+
+//        List<String> list = Arrays.asList("Jaya","Jaya","Suresh","Ramesh");
+//        System.out.println(list.stream().collect(Collectors.groupingBy(Function.identity(),Collectors.counting())));
+
+        String s="aaabcddccdcbabcddadbaabcda";
+
+        String t="bcd";
+
+        findSubStringIndexes(s,t);
+    }
+
+    public static void findSubStringIndexes(String s,String t) {
+
+         int index = s.indexOf(t);
+         boolean found = false;
+
+         while (index!=-1) {
+             System.out.println(index+" ");
+             found=true;
+
+             index=s.indexOf(t,index+1);
+         }
+
+         if (!found) {
+             System.out.println("Substrig not found");
+         }
+
+    }
+}
